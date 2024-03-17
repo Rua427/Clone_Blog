78c8bfefe5cf401f517a81f699efdf1f9747575d
Rua0427 <ekrua427@naver.com>
2024-03-17 17:33:40 +0900

    install cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8081;
 
-app.get('/test', (req, res) => {
-    res.json('test ok');
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const {userName, password} = req.body;
+
+    res.json({requestData: {userName, password}});
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const RegisterPage = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = async (ev : React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    await fetch('http://localhost:8081/register',{
+      method: 'POST',
+      body: JSON.stringify({userName, password}),
+      headers: {'Content-Type' : 'application/json'},
+
+    });
+  }
+
   return (
-    <form className='register'>
+    <form className='register' onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
+        <input  type="text" 
+                placeholder='username' 
+                value={userName} 
+                onChange={ev => setUserName(ev.target.value)} />
+        <input  type="password"
+                placeholder='password' 
+                value={password}
+                onChange={ev => setPassword(ev.target.value)}/>
         <button>Register</button>
     </form>
   )
